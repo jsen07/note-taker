479024982d8871435cb76c8305d11d8fe9a67a6a
unknown <jayssendecastro@gmail.com>
2023-04-07 23:39:50 +0100

    added HTML Routing

--- server.js
+++ server.js
@@ -2,3 +2,25 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, '/Develop/public')));
+
+//HTML ROUTING 
+app.get("/notes", function(req, res) {
+    res.sendFile(path.join(__dirname, "/Develop/public/notes.html"));
+});
+ 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./Develop/public/index.html"));
+})
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log("Application running at port: " + PORT)
+
+});
